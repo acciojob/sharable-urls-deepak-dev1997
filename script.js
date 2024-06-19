39d3872f32b2199a1702863b01a8120434acd6cf
deepak-dev1997 <108848941+deepak-dev1997@users.noreply.github.com>
2024-06-19 14:44:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // your code here
+let name = document.getElementById("name")
+let year = document.getElementById("year")
+let head= document.getElementById("url")
+let urlString="https://localhost:8080/"
+head.innerText= urlString
+
+function headChange() {
+	head.innerText="";
+	let tempUrl = urlString
+	if(name.value && year.value){
+		tempUrl= `${urlString}?name=${name.value}&year=${year.value}`
+	}
+	else if(name.value){
+		tempUrl+="?name="+name.value
+	}
+	else if (year.value){
+		tempUrl+="?year="+year.value
+	}
+	head.innerText= tempUrl
+}
